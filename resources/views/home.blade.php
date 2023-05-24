ba83cf768dc3a2a6a87f31627f9fff27309ae06d
Nicowski25 <nicola.faedo0610@gmail.com>
2023-05-24 18:41:21 +0200

    all movie info display

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,24 @@
 
 <div class="container">
     <h3>Welcome to Movies Home</h3>
-    <div class="row row-cols-1 row-cols-lg-3"></div>
-    @forelse ($movies as $movie)
+    <div class="row row-cols-3 row-cols-lg-3 g-3">
+
+        @forelse ($movies as $movie)
         <div class="col">
-            <div class="card">
+            <div class="card h-100">
                 <h3>{{ $movie->title }}</h3>
+                <p>{{ $movie->original_title }}</p>
+                <p>{{ $movie->nationality }}</p>
+                <p>{{ $movie->date }}</p>
+                <p>{{ $movie->vote }}</p>
             </div>
         </div>
-    @empty
-    <div class="col">
-        <h4>No movies found</h4>
-    </div>    
-    @endforelse
+        @empty
+        <div class="col">
+            <h4>No movies found</h4>
+        </div>    
+        @endforelse
+    </div>
 </div>
 
 @endsection
